a06103b304bc1811c6ec20db8775f38ea5993e9d
denchou <denchou617@gmail.com>
2018-08-08 12:27:40 -0400

    Fix style, texting game logic
    Added more readability to style in index.html, index.scss
    Code preliminary game logic

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,3 +9,36 @@
 $(() => {
   // your JS code goes here
 })
+
+// game logic
+
+let player = 1
+// board is array of 9 objects
+const checkWin = function (board) {
+  if ((board.box[0] === board.box[1] === board.box[2]) ||
+      (board.box[3] === board.box[4] === board.box[5]) ||
+      (board.box[6] === board.box[7] === board.box[8]) ||
+      (board.box[0] === board.box[3] === board.box[6]) ||
+      (board.box[1] === board.box[7] === board.box[4]) ||
+      (board.box[2] === board.box[5] === board.box[8]) ||
+      (board.box[0] === board.box[4] === board.box[8]) ||
+      (board.box[2] === board.box[4] === board.box[6])) {
+    return true
+  }
+  return false
+}
+// board is array of 9 objects
+const gameBoard = function (board, player) {
+  if (checkWin(board)) {
+    console.log('player', player, 'wins!')
+  } else {
+    console.log('oops')
+  }
+}
+const sample = {
+  box: ['X', 'X', 'X',
+    null, null, null,
+    null, null, null]
+}
+console.log(sample)
+gameBoard(sample, player)
